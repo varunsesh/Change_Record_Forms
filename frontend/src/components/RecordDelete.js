2c5d46221038fc7801a3825f68e23a2a046a44d3
Varun <varunsesh@gmail.com>
2023-06-10 21:10:21 +0530

    Incomplete Deleteing record component

---
+++ frontend/src/components/RecordDelete.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+export class RecordDelete extends Component {
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+        
+      }
+    }
+    
+  render() {
+    return (
+      <div>RecordDelete</div>
+    )
+  }
+}
+
+export default RecordDelete
